703043e298514a11cd889334b61c1432ad127c25
paperbenni <paperbenni@gmail.com>
2020-11-04 14:27:36 +0100

    fix speed sync

--- zsh.sh
+++ zsh.sh
@@ -39,7 +39,7 @@ autoeditvid() {
     ffmpeg -i "$1" -filter_complex \
  "highpass=f=200[frank]; [frank]lowpass=f=4000[gunter]; [gunter]compand=attacks=0:points=-80/-900|-45/-15|-27/-9|-5/-5|20/20:gain=3" "${2}tmp.mp4" && \
     auto-editor "${2}tmp.mp4" --frame_margin 3 --output_file "${2}large.mp4" && \
-    ffmpeg -i "${2}large.mp4" -filter_complex "[0:v]setpts=0.75*PTS[v];[0:a]atempo=1.5[a]" -map "[v]" -map "[a]" "$2.mp4" && \
+    ffmpeg -i "${2}large.mp4" -filter_complex "[0:v]setpts=1/1.3*PTS[v];[0:a]atempo=1.3[a]" -map "[v]" -map "[a]" "$2.mp4" && \
     rm "$1" && \
     rm "${2}large.mp4"
 }
