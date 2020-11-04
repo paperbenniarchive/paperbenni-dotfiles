74f78c2428329f49e4c660bdc29f7ef9c7f71a30
paperbenni <paperbenni@gmail.com>
2020-11-04 10:57:37 +0100

    autoeditor

--- zsh.sh
+++ zsh.sh
@@ -33,3 +33,11 @@ musiccode() {
     done
 
 }
+
+autoeditvid() {
+    ffmpeg -i "$1" -filter_complex \
+ "highpass=f=200[frank]; [frank]lowpass=f=4000[gunter]; [gunter]compand=attacks=0:points=-80/-900|-45/-15|-27/-9|-5/-5|20/20:gain=3" "${2}tmp.mp4"
+    auto-editor "${2}tmp.mp4" --frame_margin 3 --video_speed 1.2 --output_file "$2.mp4"
+    rm "$1"
+}
+
