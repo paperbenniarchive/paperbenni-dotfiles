b7ef17aaf6a17b04b85832a53dc56bb8bb9041fc
paperbenni <paperbenni@gmail.com>
2020-11-04 13:34:58 +0100

    better speed up

--- zsh.sh
+++ zsh.sh
@@ -38,8 +38,8 @@ autoeditvid() {
     [ -z "$2" ] && return 1
     ffmpeg -i "$1" -filter_complex \
  "highpass=f=200[frank]; [frank]lowpass=f=4000[gunter]; [gunter]compand=attacks=0:points=-80/-900|-45/-15|-27/-9|-5/-5|20/20:gain=3" "${2}tmp.mp4" && \
-    auto-editor "${2}tmp.mp4" --frame_margin 3 --video_speed 1.2 --output_file "${2}large.mp4" && \
-    ffmpeg -i "${2}large.mp4" "$2.mp4" && \
+    auto-editor "${2}tmp.mp4" --frame_margin 3 --output_file "${2}large.mp4" && \
+    ffmpeg -i "${2}large.mp4" -filter_complex "[0:v]setpts=0.75*PTS[v];[0:a]atempo=1.5[a]" -map "[v]" -map "[a]" "$2.mp4" && \
     rm "$1" && \
     rm "${2}large.mp4"
 }
