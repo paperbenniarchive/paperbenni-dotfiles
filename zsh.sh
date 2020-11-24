0b351090e83c421186769f205d8c827293fff2c6
paperbenni <paperbenni@gmail.com>
2020-11-24 20:05:36 +0100

    increase bitrate

--- zsh.sh
+++ zsh.sh
@@ -45,11 +45,11 @@ autoeditvid() {
     fi
 
     [ -z "$2" ] && return 1
-    ffmpeg -i "$VINPUT" -filter_complex \
+    ffmpeg -i "$VINPUT" -r 16 -filter_complex \
  "highpass=f=200[frank]; [frank]lowpass=f=4000[gunter]; [gunter]compand=attacks=0:points=-80/-900|-45/-15|-27/-9|-5/-5|20/20:gain=3" "${2}tmp.mp4" && \
     auto-editor "${2}tmp.mp4" --frame_margin 3 --output_file "${2}large.mp4" && \
 
-    ffmpeg -i "${2}large.mp4" -vcodec libx264 -b 267k -filter_complex "[0:v]setpts=1/1.3*PTS[v];[0:a]atempo=1.3[a]" -map "[v]" -map "[a]" "$2.mp4" && \
+    ffmpeg -i "${2}large.mp4" -vcodec libx264 -b 534k -filter_complex "[0:v]setpts=1/1.3*PTS[v];[0:a]atempo=1.3[a]" -map "[v]" -map "[a]" "$2.mp4" && \
 
     rm "$VINPUT" && \
     rm "${2}large.mp4"
