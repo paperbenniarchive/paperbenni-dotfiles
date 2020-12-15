bda4e59d2c36a1e33507e513439da328238ebd25
paperbenni <paperbenni@gmail.com>
2020-12-15 18:11:09 +0100

    improve melt stuff

--- zsh.sh
+++ zsh.sh
@@ -34,23 +34,39 @@ musiccode() {
 
 }
 
+preprocessvideo() {
+    [ -n "$1" ] || return 1
+    if [ -n "$2" ]
+    then
+        OUTFILE="$2"
+    else
+        OUTFILE="${1}tmp.mp4"
+    fi
+    ffmpeg -i "$1" -filter_complex \
+        "highpass=f=200[frank]; [frank]lowpass=f=4000[gunter]; [gunter]compand=attacks=0:points=-80/-900|-45/-15|-27/-9|-5/-5|20/20:gain=3" "$OUTFILE"
+}
+
 autoeditvid() {
+
+    [ -z "$2" ] && return 1
 
     if [ "$1" = "mp4" ]
     then
-        melt *.mp4 -consumer avformat:inputrender.mp4 acodec=libmp3lame vcodec=libx264 vb=8000k
-        VINPUT="inputrender.mp4"
+        mkdir tmprenders
+        for i in ./*.mp4
+        do
+            preprocessvideo "$i" "tmprender.mp4"
+            mv tmprender.mp4 tmprenders/"$i"tmp.mp4
+        done
+        melt *.mp4 -consumer avformat:"${2}tmp.mp4" acodec=libmp3lame vcodec=libx264 vb=8000k
     else
-        VINPUT="$1"
+        preprocessvideo "$1" "${2}tmp.mp4"
     fi
 
-    [ -z "$2" ] && return 1
-    ffmpeg -i "$VINPUT" -filter_complex \
- "highpass=f=200[frank]; [frank]lowpass=f=4000[gunter]; [gunter]compand=attacks=0:points=-80/-900|-45/-15|-27/-9|-5/-5|20/20:gain=3" "${2}tmp.mp4" && \
     auto-editor "${2}tmp.mp4" --frame_margin 3 --output_file "${2}large.mp4" && \
-    ffmpeg -i "${2}large.mp4" -filter_complex "[0:v]setpts=1/1.3*PTS[v];[0:a]atempo=1.3[a]" -map "[v]" -map "[a]" "$2.mp4" && \
-    rm "$VINPUT" && \
-    rm "${2}large.mp4"
+        ffmpeg -i "${2}large.mp4" -filter_complex "[0:v]setpts=1/1.3*PTS[v];[0:a]atempo=1.3[a]" -map "[v]" -map "[a]" "$2.mp4" && \
+        rm "$VINPUT" && \
+        rm "${2}large.mp4"
 }
 
 
