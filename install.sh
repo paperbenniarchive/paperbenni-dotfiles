e1e2cb3fe1022d3c8d7f9e8a9e20f571de29ca3a
paperbenni <paperbenni@gmail.com>
2021-01-18 22:52:28 +0100

    add gitconfig

--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@
 echo 'installing paperbennis personal dotfiles'
 cd dotfiles || exit 1
 instantinstall rsync
-command -v rsync &> /dev/null || exit 1
-rsync -a -r ./ ~/
+command -v rsync &>/dev/null || exit 1
+if command -v termux-setup-storage; then
+    rsync --exclude-from='termuxignore.txt' -a -r ./ ~/
+else
+    rsync -a -r ./ ~/
+fi
 echo "finished installing paperbenni's dotfiles"
-
