892843e9bdf4d1eaea7cd7e14caef82839a6e3d9
paperbenni <paperbenni@gmail.com>
2021-01-24 16:07:36 +0100

    fix termux

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd dotfiles || exit 1
 instantinstall rsync
 command -v rsync &>/dev/null || exit 1
 if command -v termux-setup-storage; then
-    rsync --exclude-from='termuxignore.txt' -a -r ./ ~/
+    rsync --exclude-from='../termuxignore.txt' -a -r ./ ~/
 else
     rsync -a -r ./ ~/
 fi
